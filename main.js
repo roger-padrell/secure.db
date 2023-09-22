e272963c8d8831b13de2bfd7be823b194cb8e771
Roger Padrell Casar <143524580+roger-padrell@users.noreply.github.com>
2023-09-22 19:41:39 +0200

    Update main.js

--- main.js
+++ main.js
@@ -10,6 +10,6 @@ class DB{
     for (var i = 0; i < dt.length; i++) {
         output += dt[i].charCodeAt(0).toString(2) + " ";
     }
-    return output;
+    return output.replaceAll("0", this.code[0]).replaceAll("1",this.code[1]);
   }
 }
